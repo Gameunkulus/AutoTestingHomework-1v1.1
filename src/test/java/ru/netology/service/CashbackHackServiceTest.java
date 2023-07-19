8f61e84c378e32d0224527fbed872e4fa00395df
Gameunkulus <bugr24-8@mail.ru>
2023-07-19 22:30:05 +0300

    program redacted 3

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        //проверка ввода обычного числа
+        int expected1 = 990;
+        Assert.assertEquals(expected1, service.remain(10));
+        //проверка ввода отрицательного числа
+        int expected2 = 1010;
+        Assert.assertEquals(expected2, service.remain(-10));
+        //проверка ввода нулевого числа
+        int expected3 = 1000;
+        Assert.assertEquals(expected3, service.remain(0));
+
+}
