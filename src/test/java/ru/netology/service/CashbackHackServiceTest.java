30ac2c403cbbc3c71c18782fd1f7f66883e9c815
Gameunkulus <bugr24-8@mail.ru>
2023-07-25 16:21:22 +0300

    program redacted 9

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,13 +4,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CashbackHackServiceTest {
-
     @Test
-    public void testRemain() {
+    public void testRemainWitCashback() {
         CashbackHackService service = new CashbackHackService();
         //проверка ввода обычного числа
         int expected = 100;
         Assertions.assertEquals(service.remain(900), expected);
+    }
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        Assertions.assertEquals(service.remain(1000), expected);
     }
 
 }
