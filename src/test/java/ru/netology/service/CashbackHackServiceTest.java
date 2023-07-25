f216c135442628d2d29c7783c1c9846970615947
Gameunkulus <bugr24-8@mail.ru>
2023-07-25 16:19:41 +0300

    program redacted 9

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,12 +4,19 @@ import org.testng.Assert;
 
 
 public class CashbackHackServiceTest {
-
     @org.testng.annotations.Test
-    public void testRemain() {
+    public void testRemainWitCashback() {
         CashbackHackService service = new CashbackHackService();
         //проверка ввода обычного числа
         int expected = 100;
         Assert.assertEquals(service.remain(900), expected);
     }
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        Assert.assertEquals(service.remain(1000), expected);
+    }
+
 }
